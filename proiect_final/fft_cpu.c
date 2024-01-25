aa622cc6ac9b8b15b442bfc59cef94cc65004262
Radu Moraru <randreimoraru@gmail.com>
2024-01-25 16:07:55 +0100

    Fix memory leaks and segfaults.

--- proiect_final/fft_cpu.c
+++ proiect_final/fft_cpu.c
@@ -4,14 +4,14 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define SHAPE_FILENAME "resources/poza_rgb.shape"
-#define IMG_R_FILENAME "resources/poza_rgb.r"
-#define IMG_G_FILENAME "resources/poza_rgb.g"
-#define IMG_B_FILENAME "resources/poza_rgb.b"
+#define SHAPE_FILENAME "resources/img.shape"
+#define IMG_R_FILENAME "resources/img_r.mat"
+#define IMG_G_FILENAME "resources/img_g.mat"
+#define IMG_B_FILENAME "resources/img_b.mat"
 
-#define FFT_R_FILENAME "output/fft.r"
-#define FFT_G_FILENAME "output/fft.g"
-#define FFT_B_FILENAME "output/fft.b"
+#define FFT_R_FILENAME "output/fft_r.mat"
+#define FFT_G_FILENAME "output/fft_g.mat"
+#define FFT_B_FILENAME "output/fft_b.mat"
 
 #define pi 3.14159265
 
@@ -72,21 +72,25 @@ int get_color(uint8_t *image_color, const uint32_t rows, const uint32_t cols, co
     return 0;
 }
 
-int write_matrix(uint8_t *matrix, const uint32_t rows, const uint32_t cols, const char *matrix_file) {
+int write_fft(float *matrix, const uint32_t rows, const uint32_t cols, const char *matrix_file) {
     FILE *fileptr;
     size_t objects_written;
 
     fileptr = fopen(matrix_file, "wb");
     if (fileptr == NULL) {
+        fprintf(stderr, "Unable to open %s.\n", matrix_file);
         return -1;
     }
 
     objects_written = fwrite(matrix, sizeof(*matrix), rows*cols, fileptr);
+    printf("Wrote %d floats of size %d to %s. total bytes written: %d.\n", objects_written, sizeof(*matrix), matrix_file);
     if (objects_written != rows*cols) {
+        fprintf(stderr, "Could not read all bytes from %s. Bytes read: %d, Expected: %d\n", matrix_file, objects_written, rows*cols);
         return -1;
     }
 
     if (fclose(fileptr) != 0) {
+        fprintf(stderr, "Error closing %s.\n", matrix_file);
         return -1;
     }
     return 0;
@@ -101,11 +105,14 @@ int is_power_of_2(uint32_t num) {
     return popcount == 1;
 }
 
-void swap_float(float_t *a, float_t *b) {
+void swap_float(float *a, float *b) {
     char *a_bytes, *b_bytes;
     if (*a == *b) {
         return;
     }
+    a_bytes = (char *)a;
+    b_bytes = (char *)b;
+
     for (int i = 0; i < sizeof(*a); i++) {
         a_bytes[i] ^= b_bytes[i];
         b_bytes[i] ^= a_bytes[i];
@@ -132,10 +139,16 @@ uint32_t log_of_pow_of_2(uint32_t num) {
     }
 }
 
-int complex_fft(float_t *real, float_t *imag, const uint32_t N) {
-    float_t *a_real, *a_imag;
+void scale(float *row, const uint32_t N, const float scalar) {
+    for (int i = 0; i < N; i++) {
+        row[i] *= scalar;
+    }
+}
+
+int complex_fft(float *real, float *imag, const uint32_t N) {
+    float *a_real, *a_imag;
     uint32_t u, pow_2_i;
-    float_t iterations, sin_u, cos_u;
+    float iterations, sin_u, cos_u;
 
     a_real = malloc(N*sizeof(*a_real));
     if (a_real == NULL) {
@@ -153,10 +166,12 @@ int complex_fft(float_t *real, float_t *imag, const uint32_t N) {
             u = n / (N/pow_2_i);
             sin_u = sin(-2*u*pi*pow_2_i);
             cos_u = cos(-2*u*pi*pow_2_i);
-            a_real[n] = real[n+u*N/pow_2_i] + real[n+u*N/pow_2_i+N/pow_2_i]*cos_u +
-                imag[n+u*N/pow_2_i+N/pow_2_i]*sin_u;
-            a_imag[n] = imag[n+u*N/pow_2_i] + imag[n+u*N/pow_2_i+N/pow_2_i]*cos_u +
-                real[n+u*N/pow_2_i+N/pow_2_i]*sin_u;
+            a_real[n] = real[(n + u*N/pow_2_i) % N] +
+                        real[(n + u*N/pow_2_i + N/pow_2_i) % N]*cos_u +
+                        imag[(n + u*N/pow_2_i + N/pow_2_i) % N]*sin_u;
+            a_imag[n] = imag[(n + u*N/pow_2_i) % N] +
+                        imag[(n + u*N/pow_2_i + N/pow_2_i) % N]*cos_u +
+                        real[(n + u*N/pow_2_i + N/pow_2_i) % N]*sin_u;
         }
 
         for(int n = 0; n < N; n++) {
@@ -164,11 +179,20 @@ int complex_fft(float_t *real, float_t *imag, const uint32_t N) {
             imag[n] = a_imag[n];
         }
     }
+
+    scale(real, N, 1/(float)N);
+    scale(imag, N, 1/(float)N);
+
+    free(a_real);
+    free(a_imag);
 }
 
-void detangle_and_pack(float_t *row1, float_t *row2, const uint32_t N) {
+void detangle_and_pack(float *row1, float *row2, const uint32_t N) {
     register uint8_t Fr, Fi, Gr, Gi;
     for (int i = 0; i < N; i++) {
+        if (i == 0 || i == N/2) {
+            continue;
+        }
         Fr = (row1[i] + (uint32_t)row1[N-i]) / 2;
         Fi = (row2[i] - (uint32_t)row2[N-i]) / 2;
         Gr = (row2[i] + (uint32_t)row2[N-i]) / 2;
@@ -181,12 +205,12 @@ void detangle_and_pack(float_t *row1, float_t *row2, const uint32_t N) {
     }
 }
 
-void real_pair_fft(float_t *row1, float_t *row2, const uint32_t len) {
+void real_pair_fft(float *row1, float *row2, const uint32_t len) {
     complex_fft(row1, row2, len);
     detangle_and_pack(row1, row2, len);
 }
 
-void transpose(float_t *matrix, uint32_t *rows, uint32_t *cols) {
+void transpose(float *matrix, uint32_t *rows, uint32_t *cols) {
     for (int i = 0; i < *rows; i++) {
         for (int j = 0; j < *cols; j++) {
             swap_float(matrix + i*(*cols) + j, matrix + j*(*rows) + i);
@@ -195,7 +219,7 @@ void transpose(float_t *matrix, uint32_t *rows, uint32_t *cols) {
     swap_int(rows, cols);
 }
 
-int matrix_fft(float_t *matrix, uint32_t rows, uint32_t cols) {
+int matrix_fft(float *matrix, uint32_t rows, uint32_t cols) {
     if (!is_power_of_2(rows) || !is_power_of_2(cols)) {
         fprintf(stderr, "The image dimensions must be a power of 2.\n");
         return -1;
@@ -213,18 +237,21 @@ int matrix_fft(float_t *matrix, uint32_t rows, uint32_t cols) {
     }
 
     transpose(matrix, &rows, &cols);
+
+    return 0;
 }
 
 int main() {
     uint32_t rows, cols;
     uint8_t *image_r, *image_g, *image_b;
-    float_t *image_rf, *image_gf, *image_bf;
+    float *image_rf, *image_gf, *image_bf;
 
     if (read_shape(&rows, &cols) != 0) {
         fprintf(stderr, "Unable to read image shape.");
         return -1;
     }
-    
+
+    printf("Allocating memory.\n");
     image_r = malloc(rows*cols*sizeof(*image_r));
     if (image_r == NULL) {
         return -1;
@@ -268,12 +295,22 @@ int main() {
     if (matrix_fft(image_rf, rows, cols) != 0 ||
             matrix_fft(image_gf, rows, cols) != 0 ||
             matrix_fft(image_bf, rows, cols) != 0) {
+        fprintf(stderr, "Error calculating fft.\n");
         return -1;
     }
+
+    printf("writing.\n");
+    write_fft(image_rf, rows, cols, FFT_R_FILENAME);
+    write_fft(image_gf, rows, cols, FFT_G_FILENAME);
+    write_fft(image_bf, rows, cols, FFT_B_FILENAME);
 
     free(image_r);
     free(image_g);
     free(image_b);
+
+    free(image_rf);
+    free(image_gf);
+    free(image_bf);
 
     return 0;
 }
--- proiect_final/jpg_to_rgb.py
+++ proiect_final/jpg_to_rgb.py
@@ -6,7 +6,7 @@ print(type(img[:, :, 0].shape[0]))
 
 cropped_rows, cropped_cols = 2048, 2048
 
-with open('resources/poza_rgb.shape', "bw+") as f:
+with open('resources/img.shape', "bw+") as f:
     f.write(cropped_rows.to_bytes(4, 'little'))
     f.write(cropped_cols.to_bytes(4, 'little'))
 
@@ -16,6 +16,6 @@ img_r = img[rows//2-1024:rows//2+1024, cols//2-1024:cols//2+1024, 0].copy().asty
 img_g = img[rows//2-1024:rows//2+1024, cols//2-1024:cols//2+1024, 1].copy().astype(dtype=np.uint8)
 img_b = img[rows//2-1024:rows//2+1024, cols//2-1024:cols//2+1024, 2].copy().astype(dtype=np.uint8)
 
-img_r.tofile('resources/poza_rgb.r')
-img_g.tofile('resources/poza_rgb.g')
-img_b.tofile('resources/poza_rgb.b')
+img_r.tofile('resources/img_r.mat')
+img_g.tofile('resources/img_g.mat')
+img_b.tofile('resources/img_b.mat')
